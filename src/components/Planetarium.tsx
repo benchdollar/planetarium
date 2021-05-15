a17b91224dde35c45e826d0fe8b7b14aac585b1c
Benjamin Knoop <knoop@safelog.de>
2021-05-16 01:08:00 +0200

    Package 'axios' with API for HTTP GET and PUT

--- src/components/Planetarium.tsx
+++ src/components/Planetarium.tsx
@@ -5,6 +5,9 @@ import PlanetariumCard from "./PlanetariumCard";
 import { PlanetariumItems } from "./PlanetariumData";
 import {PlanetariumContext} from "./PlanetariumContext";
 import { useSnackbar } from 'notistack';
+import axios from "axios";
+
+const apiUrl = 'http://localhost:3001/planetarium/state';
 
 const noOfSwitches = 18;
 const initialState: Array<boolean> = Array(noOfSwitches).fill(false);
@@ -46,13 +49,65 @@ const Planetarium = () => {
             state[id] = switchedOn;
             setSwitches(state);
             count++;
-            console.log("YYYYYYY");
-            // handleClick();
+            console.log("state: " +  state2string(switches));
+            // TODO: unit test
+            console.log("state: " + state2string(string2state(state2string(switches))) + " (unit test)");
+
+            // api put
+            apiPut(switches);
+            
         }
     };
 
+    const state2string = (state : boolean[]) => {
+        return state.map((value, i) => (value ? "1" : "0")).join("");
+    }
+
+    const string2state = (input : string) => {
+        if (input.length !== noOfSwitches) {
+            console.log("Error unpacking state from string representation");
+            return [];
+        }
+        let state = Array<boolean>();
+        for(let i = 0; i < input.length; ++i) {
+            state.push(input.charAt(i) === '1' ? true : false);
+        }
+        return state;
+    }
+
+    const apiGet = () => {
+        axios.get(apiUrl)
+            .then(response => {
+                console.log(response.data);
+                enqueueSnackbar(`Loaded planetarium state from API`, {
+                    variant: 'success',
+                });
+            })
+            .catch((reason:any) => {
+                enqueueSnackbar(`Failed to load planetarium state from API: ${reason}`, {
+                    variant: 'error',
+                });
+            });
+    }
+
+    const apiPut = (state : boolean[]) => {
+        axios.put(apiUrl, { "state" : state2string(state)})
+            .then(response => {
+                console.log(response.data);
+                enqueueSnackbar(`Set planetarium state with API`, {
+                    variant: 'success',
+                });
+            })
+            .catch((reason: any) => {
+                enqueueSnackbar(`Failed to load planetarium state from API: ${reason}`, {
+                    variant: 'error',
+                });
+            });
+    }
+
     useEffect(() => {
         console.log("XXXXXXX");
+        apiGet();
         // return () => {
             
         // }
